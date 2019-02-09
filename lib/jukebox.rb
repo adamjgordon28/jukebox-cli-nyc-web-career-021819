68ccefff1c658259f44b3d776a2749db2cc63c5f
Adam Gordon <adamjgordon24@gmail.com>
2019-02-09 19:25:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,25 +55,6 @@ end
 
 def run(songs)
   help
-  command = "" 
-  while command  
-  puts "Please enter a command:"
-  command = gets.strip  
-  case command 
-    when 'list'
-      list(songs)
-      when 'play'
-        list(songs)
-        play(songs)
-      when 'help'
-        help 
-      when 'exit'
-        exit_jukebox
-        break 
-      else 
-        help 
-      end 
-    end 
-  end 
+end
 
 run(songs)
